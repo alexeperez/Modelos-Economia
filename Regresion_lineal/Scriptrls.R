d5039e0066863d2c6543833ab703fe5fd783347d
lexperez90@hotmail.com <lexperez90@hotmail.com>
2015-10-28 16:09:55 -0500

    Update regresion limeal multiple

--- Regresion_lineal/Scriptrls.R
+++ Regresion_lineal/Scriptrls.R
@@ -1,8 +1,7 @@
 #-----------------------------------------------------#
-#               Modelos en Economia                   #
+#               Modelos en Economia
 # Alex E. Pérez 
-# MS-PLUS Consultores
-# http://www.consultoresmatematicos.com
+# Escuela Politécnica Nacional
 #-----------------------------------------------------#
 
 # directorio de trabajo
@@ -21,6 +20,7 @@ colnames(datarls)
 summary(datarls)
 
 # grafico de dispersion
+# Variables
 utilidad <- datarls[,"Utilidad"]
 ventas <- datarls[,"Ventas"]
 
@@ -39,27 +39,29 @@ g + geom_point(size=4, color="black")
 # Correlacion
 cor(x = utilidad, y= ventas)
 
-# Regresion lineal
-reg1 <- lm(utilidad ~ ventas, data=datarls)
-summary(reg1)
+#-----------------------------------------------------#
+#              Regresion lineal Simple
+
+regs <- lm(utilidad ~ ventas)
+summary(regs)
 
 # ANOVA
-anova1 <- aov(reg1)
-summary(anova1)
+anovas <- aov(regs)
+summary(anovas)
 
-# Objeto reg1
-str(reg1)
-names(reg1)
+# Objeto regs
+str(regs)
+names(regs)
 
 # Residuos
-u_t <- reg1$residuals
+u_t <- regs$residuals
 
 # Hipotesis sobre los errores
 mean(u_t)
 hist(u_t)
 
 # Pronosticos o predicciones
-y_t <- reg1$fitted.values
+y_t <- regs$fitted.values
 
 datafinal <- data.frame(datarls, y_t)
 View(datafinal)
@@ -73,4 +75,61 @@ g + geom_point(size=3, color="black")+
 
 # DEBER
 # Ajustar un modelo de regresion lineal simple entre
-# pib e inflacion del archivo data_rls_pib.xlsx
+# ln(pib) e ln(inflacion) del archivo data_rls_pib.xlsx
+# use la funcion log() para extraer el log natural de una variable
+
+
+#-----------------------------------------------------#
+#              Regresion lineal multiple
+
+# data_rls_uti.xlsx
+datarlm <- read_excel("data_rlm_pib.xlsx", sheet = 1, col_names = TRUE, na = "")
+View(datarlm)
+str(datarlm)
+colnames(datarlm)
+summary(datarlm)
+
+# Variables
+pib <- datarlm[,"pib"]
+infl <- datarlm[,"inflacion"]
+ee <- datarlm[,"ee"]
+
+# grafico de dispersion
+# variable inflacion
+plot(x = infl,y = pib,main = "pib vs inflacion",
+     pch=16, col="blue")
+
+# variable ee
+plot(x = ee,y = pib,main = "pib vs ee",
+     pch=16, col="blue")
+
+# Relacion no lineal correccion
+# transformar variables
+ln_pib <- log(pib)
+ln_infl <- log(infl)
+ln_ee <- log(ee)
+
+plot(x = ln_infl, y = ln_pib,main = "ln_pib vs ln_infl",
+     pch=16, col="blue")
+
+plot(x = ln_ee,y = ln_pib, main = "pib vs ee",
+     pch=16, col="blue")
+
+
+regm <- lm(ln_pib ~ ln_infl+ ln_ee)
+summary(regm)
+
+# ANOVA
+anovam <- aov(regm)
+summary(anovam)
+
+# Objeto regm
+names(regm)
+
+# Pronosticos o predicciones
+y_t <- regm$fitted.values
+
+datafinal <- data.frame(datarlm,ln_pib, y_t)
+View(datafinal)
+
+
